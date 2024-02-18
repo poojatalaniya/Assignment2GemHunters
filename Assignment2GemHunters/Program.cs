92552e8a916b78b2408d045ee003d44a59192063
Pooja Talaniya <pooja@FLEXI>
2024-02-18 18:43:22 -0500

    Beginning of the code

--- Assignment2GemHunters/Program.cs
+++ Assignment2GemHunters/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Assignment2-Game Hunters
+//By Pooja Talaniya - Ptalaniya4569@conestogac.on.ca - 8904569
+//Beginning of the Code
+
+using System;
+
+//A Class is defined named Position
+class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
