26a01baa3f4fecbfe8b067cedce0cc1c0229e956
Pooja Talaniya <pooja@FLEXI>
2024-02-19 00:37:41 -0500

    Adding Switch statement

--- Assignment2GemHunters/Program.cs
+++ Assignment2GemHunters/Program.cs
@@ -16,4 +16,37 @@ class Position
         Y = y;
     }
 }
+class Player
+{
+    public string Name { get; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+    }
+    public void Move(char direction)
+    {
+        switch (direction)
+        {
+            case 'U':
+                Position.Y--;
+                break;
+            case 'D':
+                Position.Y++;
+                break;
+            case 'L':
+                Position.X--;
+                break;
+            case 'R':
+                Position.X++;
+                break;
+            default:
+                break;
+        }
+    }
+}
 
